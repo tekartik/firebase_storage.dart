bd93d07979cc894b8d6357d95238374b4d738bd5
Alexandre Roux <alex@tekartik.com>
2018-11-04 15:48:45 +0100

    feat: start storage browser

---
+++ storage_browser/test/storage_browser_test.dart
@@ -0,0 +1,21 @@
+@TestOn('browser')
+library tekartik_firebase_storage_browser.storage_browser_test;
+
+import 'package:tekartik_firebase_browser/firebase_browser.dart';
+import 'package:tekartik_firebase_storage_browser/storage_browser.dart';
+import 'package:tekartik_firebase_storage_test/storage_test.dart';
+import 'package:test/test.dart';
+
+import 'test_setup.dart';
+
+void main() async {
+  var options = await setup();
+  if (options == null) {
+    return;
+  }
+  var firebase = firebaseBrowser;
+
+  group('browser', () {
+    run(firebase: firebase, storageService: storageService, options: options);
+  });
+}
