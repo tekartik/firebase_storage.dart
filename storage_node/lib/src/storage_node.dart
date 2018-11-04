b31c8175469ef7fade44000de986d65037dce0b5
Alexandre Roux <alex@tekartik.com>
2018-11-04 11:38:31 +0100

    feat: storage node

---
+++ storage_node/lib/src/storage_node.dart
@@ -0,0 +1,100 @@
+import 'dart:async';
+import 'package:js/js_util.dart';
+import 'package:node_interop/util.dart';
+import 'package:tekartik_common_utils/common_utils_import.dart';
+import 'package:tekartik_firebase/firebase.dart';
+import 'package:tekartik_firebase_storage_node/src/storage_bindings.dart'
+    as native;
+import 'package:tekartik_firebase_storage/storage.dart';
+import 'package:tekartik_firebase_node/impl/firebase_node.dart';
+
+class StorageServiceNode implements StorageService {
+  final _storages = <AppNode, StorageNode>{};
+
+  StorageServiceNode();
+
+  @override
+  Storage storage(App app) {
+    assert(app is AppNode, 'invalid firebase app type');
+    AppNode appNode = app;
+    StorageNode storage = _storages[appNode];
+    if (storage == null) {
+      // ignore: invalid_use_of_protected_member
+      var storageApp = (appNode.nativeInstance).nativeInstance;
+      storage = StorageNode((storageApp as native.StorageApp).storage());
+      _storages[appNode] = storage;
+    }
+    return storage;
+  }
+}
+
+StorageServiceNode _storageServiceNode;
+StorageServiceNode get storageServiceNode =>
+    _storageServiceNode ??= StorageServiceNode();
+
+class StorageNode implements Storage {
+  final native.Storage nativeInstance;
+
+  StorageNode(this.nativeInstance);
+
+  @override
+  Bucket bucket([String name]) {
+    native.Bucket nativeBucket;
+    if (name == null) {
+      nativeBucket = callMethod(nativeInstance, "bucket", []) as native.Bucket;
+    } else {
+      nativeBucket = nativeInstance.bucket(name);
+    }
+    return _wrapBucket(nativeBucket);
+  }
+}
+
+class FileNode implements File {
+  final native.File nativeInstance;
+
+  FileNode(this.nativeInstance);
+
+  @override
+  Future save(data) => promiseToFuture(nativeInstance.save(data));
+
+  @override
+  Future<bool> exists() async {
+    // Array with first bool as the response
+    var fileExistsResponse =
+        (await promiseToFuture(nativeInstance.exists())) as List;
+    return fileExistsResponse[0] as bool;
+  }
+
+  @override
+  Future<List<int>> download() async {
+    // Array with first item as the response
+    var downloadResponse =
+        (await promiseToFuture(nativeInstance.download())) as List;
+    return downloadResponse[0] as List<int>;
+  }
+
+  @override
+  Future delete() => promiseToFuture(nativeInstance.delete());
+}
+
+class BucketNode implements Bucket {
+  final native.Bucket nativeInstance;
+
+  BucketNode(this.nativeInstance);
+
+  @override
+  File file(String path) => _wrapFile(nativeInstance.file(path));
+
+  @override
+  Future<bool> exists() =>
+      promiseToFuture(nativeInstance.exists()).then((data) => data[0] as bool);
+
+  @override
+  String get name => nativeInstance.name;
+}
+
+BucketNode _wrapBucket(native.Bucket nativeInstance) =>
+    nativeInstance != null ? BucketNode(nativeInstance) : null;
+
+FileNode _wrapFile(native.File nativeInstance) =>
+    nativeInstance != null ? FileNode(nativeInstance) : null;
---
+++ storage_node/lib/storage_node.dart
@@ -0,0 +1,4 @@
+import 'package:tekartik_firebase_storage/storage.dart';
+import 'package:tekartik_firebase_storage_node/src/storage_node.dart' as _;
+
+StorageService get storageService => _.storageServiceNode;
