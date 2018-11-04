b8e8cc24b12297915ee806600b21353e69ce2dce
Alexandre Roux <alex@tekartik.com>
2018-11-04 12:50:48 +0100

    fix: test

---
+++ storage/test/storage_test.dart
@@ -0,0 +1,16 @@
+library tekartik_firebase_storage.storage__test;
+
+import 'package:tekartik_firebase_storage/storage.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('storage', () {
+    test('api', () {
+      // Exported
+      Storage;
+      File;
+      Bucket;
+      StorageService;
+    });
+  });
+}
