4b16c4999d5847ef73ddda0f5fa499a64db89b57
alex <alex@tekartik.com>
2019-07-18 15:15:37 +0200

    build: dependencies

---
+++ tool/travis.dart
@@ -0,0 +1,22 @@
+import 'package:process_run/shell.dart';
+
+Future main() async {
+  var shell = Shell();
+
+  for (var dir in [
+    'storage',
+    'storage_browser',
+    'storage_fs',
+    'storage_node',
+    // 'storage_test',
+  ]) {
+    shell = shell.pushd(dir);
+    await shell.run('''
+    
+    pub get
+    dart tool/travis.dart
+    
+''');
+    shell = shell.popd();
+  }
+}
