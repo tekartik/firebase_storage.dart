6dfdc4f25db8d6398b6420d881d065861d11dc0b
alex <alex@tekartik.com>
2021-08-23 19:04:09 +0200

    feat: add newStorageMemory() helper for unit test

--- storage_fs/lib/storage_fs.dart
+++ storage_fs/lib/storage_fs.dart
@@ -1,3 +1,4 @@
+import 'package:tekartik_firebase_local/firebase_local.dart';
 import 'package:tekartik_firebase_storage/storage.dart';
 import 'package:tekartik_firebase_storage_fs/src/storage_fs.dart' as impl;
 
@@ -6,3 +7,7 @@ StorageService get storageServiceMemory => impl.storageServiceFsMemory;
 
 /// Create an empty in memory service (mainly for unit tests)
 StorageService newStorageServiceMemory() => impl.newStorageServiceFsMemory();
+
+/// Quick storage test helper
+Storage newStorageMemory() =>
+    newStorageServiceMemory().storage(FirebaseLocal().app());
